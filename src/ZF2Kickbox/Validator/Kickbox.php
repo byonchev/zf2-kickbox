3ca019ee70805529b08f45ffde2c056744cf2a8e
Boris Yonchev <boris@yonchev.me>
2015-10-30 12:05:40 +0200

    Added options to the validator

--- src/ZF2Kickbox/Validator/Kickbox.php
+++ src/ZF2Kickbox/Validator/Kickbox.php
@@ -4,7 +4,10 @@ namespace ZF2Kickbox\Validator;
 
 use Kickbox\Client;
 use Kickbox\HttpClient\Response;
+use Traversable;
+use Zend\Stdlib\ArrayUtils;
 use Zend\Validator\AbstractValidator;
+use Zend\Validator\Exception\InvalidArgumentException;
 
 /**
  * @author      Boris Yonchev <boris@yonchev.me>
@@ -16,43 +19,142 @@ class Kickbox extends AbstractValidator
     const RESULT_RISKY         = 'risky';
     const RESULT_UNKNOWN       = 'unknown';
 
+    const INVALID   = 'invalidEmail';
+    const NOT_SAFE  = 'notSafe';
+    const EXCEPTION = 'exception';
+
+    /**
+     * @var array
+     */
+    protected $messageTemplates = [
+        self::NOT_SAFE  => "Email address cannot be verified",
+        self::INVALID   => "Email address rejected",
+        self::EXCEPTION => "There was an error validating the email address"
+    ];
+
+    /**
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * @var bool
+     */
+    protected $strictMode;
+
+    /**
+     * Sets validator options
+     *
+     * @param  array|Traversable $options
+     *
+     * @throws InvalidArgumentException
+     */
     public function __construct($options = null)
     {
+        if ($options instanceof Traversable) {
+            $options = ArrayUtils::iteratorToArray($options);
+        }
+
+        if (!array_key_exists('apiKey', $options)) {
+            throw new InvalidArgumentException("Missing Kickbox API key");
+        }
+
+        if (!array_key_exists('strictMode', $options)) {
+            $options['strictMode'] = false;
+        }
+
+        $this->setApiKey($options['apiKey'])->setStrictMode($options['strictMode']);
+
         parent::__construct($options);
     }
 
+    /**
+     * @return boolean
+     */
+    public function isStrictMode()
+    {
+        return $this->strictMode;
+    }
+
+    /**
+     * @param boolean $strictMode
+     *
+     * @return $this
+     */
+    public function setStrictMode($strictMode)
+    {
+        $this->strictMode = $strictMode;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @param string $apiKey
+     *
+     * @return $this
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return bool
+     */
     public function isValid($value)
     {
-        $client  = new Client('');
-        $kickbox = $client->kickbox();
+        $this->setValue($value);
+
+        $strictMode = $this->isStrictMode();
 
         try {
+            $client = new Client($this->getApiKey());
+
+            $kickboxClient = $client->kickbox();
+
             /* @var Response $response */
-            $response = $kickbox->verify($value);
+            $response = $kickboxClient->verify($value);
 
             $this->logResponse($response);
 
-            return $this->getBooleanResult($response->body['result']);
+            $result = $response->body['result'];
+
+            if ($strictMode) {
+                if ($result === self::RESULT_UNDELIVERABLE) {
+                    $this->error(self::INVALID);
+
+                    return false;
+                } else if ($result !== self::RESULT_DELIVERABLE) {
+                    $this->error(self::NOT_SAFE);
+
+                    return false;
+                }
+            } else if ($result === self::RESULT_UNDELIVERABLE) {
+                $this->error(self::INVALID);
+
+                return false;
+            }
         } catch (\Exception $e) {
+            $this->error(self::EXCEPTION);
+
             $this->logError($e);
 
             return false;
         }
-    }
 
-    /**
-     * @param string $result
-     * @param bool   $strictMode
-     *
-     * @return bool
-     */
-    private function getBooleanResult($result, $strictMode = false)
-    {
-        if ($strictMode) {
-            return $result === self::RESULT_DELIVERABLE;
-        } else {
-            return $result !== self::RESULT_UNDELIVERABLE;
-        }
+        return true;
     }
 
     protected function logError(\Exception $e)
